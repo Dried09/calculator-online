7fa90bc9ecf9f7bd6467799b24e19290e253197a
Dried09 <driedsf@gmail.com>
2017-06-22 20:23:11 +0300

    Init. Simple operations & View.

---
+++ Calc/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Calc.Models;
+
+namespace Calc.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string result = "Shit";
+
+            return result;
+        }
+
+        [HttpPost]
+        public double Post([FromBody] Operation operation)
+        {
+            double result = 0;
+
+            switch (operation.Action)
+            {
+                case "add":
+                    result = operation.FirstNumber + operation.SecondNumber;
+                    break;
+                case "substract":
+                    result = operation.FirstNumber - operation.SecondNumber;
+                    break;
+                case "multiply":
+                    result = operation.FirstNumber * operation.SecondNumber;
+                    break;
+                case "divide":
+                    result = operation.FirstNumber / operation.SecondNumber;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Calc/Models/Operation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calc.Models
+{
+    public class Operation
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public string Action { get; set; }
+    }
+}
